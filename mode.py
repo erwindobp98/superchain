eb01afdcae79e4663a5eb442611688869e8e8d81
WEEN <80139938+erwindobp98@users.noreply.github.com>
2025-06-27 15:32:05 +0700

    Update mode.py

--- mode.py
+++ mode.py
@@ -9,8 +9,8 @@ from colorama import init, Fore, Style
 init(autoreset=True)
 
 # Connect to Taiko RPC
-taiko_rpc_url = "https://mode.drpc.org"
-web3 = Web3(Web3.HTTPProvider(taiko_rpc_url))
+mode_rpc_url = "https://mode.drpc.org"
+web3 = Web3(Web3.HTTPProvider(mode_rpc_url))
 
 # Check network connection
 if not web3.is_connected():
